b84167ca65e3c902c285d507728df0f829473ea0
J Hon <jx@blackwingedcreatives.com>
2019-02-06 14:01:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(){
   var johnLennonShouts = new Array();
-  while (johnLennonFacts.length > 0 ){ //while johnLennonFacts[] is shorter than the array length johnLennonFacts.length 
+  while (johnLennonFacts.length > 0 ){ //as long as the array length is bigger than 0 johnLennonFacts.length 
     johnLennonShouts.push(`${johnLennonFacts[n]}!!!`)
   }
   return johnLennonShouts;

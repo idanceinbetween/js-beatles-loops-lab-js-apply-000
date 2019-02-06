05d5bae2cf9972d2e81fae3e2c546d605f5d7591
J Hon <jx@blackwingedcreatives.com>
2019-02-06 14:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,14 +20,9 @@ function johnLennonFacts(facts){ //This function will accept one argument, an ar
 function iLoveTheBeatles(n){ //accepts number as a parameter
   var loveThem = new Array() // The function should create a variable that stores an empty array.
 
-  var n = 0
-  function incrementN(){
-    n = n + 1
-  }
-
   do{
     loveThem.push(`I love the Beatles!`)
-    incrementN();//The loop should then increment the number passed in as a parameter.
+    n++//The loop should then increment the number passed in as a parameter.
   } while(n < 15 ) // The first check is when n = 2, As long as the parameter number is less than 15, then
 //The condition of the loop should check to see that the parameter number is less than 15.
 // It should contain a number of strings that represents the difference between the input number and 15.
